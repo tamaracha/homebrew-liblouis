718b6bba0bbf5adbebc6ff56e3ec8af13cf954c9
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-11-12 22:40:57 +0100

    Liblouisutdml 2.8

--- liblouisutdml.rb
+++ liblouisutdml.rb
@@ -1,21 +1,33 @@
-require 'formula'
-
 class Liblouisutdml < Formula
-  homepage 'http://code.google.com/p/liblouisutdml'
-  url 'https://liblouisutdml.googlecode.com/files/liblouisutdml-2.5.0.tar.gz'
-  version '2.5.0'
-  sha1 'ea2be45be601e2db4ba0757f87febccf9d507ea8'
+  desc "Braille  transcription services for xml, html and text documents"
+  homepage "http://liblouis.org"
+  url "https://github.com/liblouis/liblouisutdml/releases/download/v2.8.0/liblouisutdml-2.8.0.tar.gz"
+  sha256 "97f0ecb0182f51891704bf545c92e3cc705735e86edb9b2de74027ce167760a6"
 
-  depends_on 'liblouis'
-  depends_on 'libxml2'
-  depends_on 'pkg-config' => :build
+  depends_on "help2man" => :build
+  depends_on "pkg-config" => :build
+  depends_on "liblouis"
+  uses_from_macos "libxml2"
 
   def install
-    ENV['CFLAGS'] = '-I/System/Library/Frameworks/JavaVM.framework/Headers'
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test liblouisutdml`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
