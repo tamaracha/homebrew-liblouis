5a17017206a60adaa8a5b543013f4871211951c5
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-11-12 12:40:08 +0100

    Build manpages with help2man

--- liblouis.rb
+++ liblouis.rb
@@ -5,16 +5,16 @@ class Liblouis < Formula
   stable do
     url "https://github.com/liblouis/liblouis/releases/download/v3.11.0/liblouis-3.11.0.tar.gz"
     sha256 "b802aba0bff49636907ca748225e21c56ecf3f3ebc143d582430036d4d9f6259"
+    depends_on "help2man" => :build
     depends_on "pkg-config" => :build
-    depends_on "python" => :optional
   end
   head do
     url "https://github.com/liblouis/liblouis.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
-    depends_on "Libtool" => :build
+    depends_on "help2man" => :build
+    depends_on "libtool" => :build
     depends_on "pkg-config" => :build
-    depends_on "python" => :optional
   end
 
   option "with-python", "compile with Python bindings"
